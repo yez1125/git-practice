9232e652e1fdf927e44eebf7ad97c6323679b648
yez1125 <yez1125lin@gmail.com>
2024-09-23 21:18:14 +0800

    完成sum.js 挑戰二

--- week-02/sum.js
+++ week-02/sum.js
@@ -12,3 +12,29 @@ function sum(ary) {
 
 //test
 console.log(sum([1, 5, 3, 2, 9, -10])); // 10
+
+//挑戰二
+
+//公式解
+function sum(num) {
+  if (num >= 0) {
+    return ((1 + num) * num) / 2;
+  } else {
+    return "請輸入0或正整數";
+  }
+}
+
+//遞迴
+function sum(num) {
+  if (num >= 0) {
+    if (num == 1) {
+      return 1;
+    } else {
+      return num + sum(num - 1);
+    }
+  } else {
+    return "請輸入0或正整數";
+  }
+}
+//test
+//console.log(sum(12));
